c3daa1bffc105102ad6e49927a9dfd3cdf77b0b8
yy <yy@maomao.com>
2022-11-04 16:57:38 +0800

    完成前端登录注册相关的界面

--- book-mgr-fe/src/router/index.js
+++ book-mgr-fe/src/router/index.js
@@ -1,17 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-  },
+    path: '/auth',
+    name: '/Auth',
+    component: () => import(/* webpackChunkName: "auth" */ '../views/Auth/index.vue')
+  }
 ];
 
 const router = createRouter({
