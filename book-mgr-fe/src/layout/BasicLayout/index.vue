e79c9a396bcd5e0988a62bafbac1a82dab8890c5
yy <yy@maomao.com>
2022-11-05 15:07:56 +0800

    测试

---
+++ book-mgr-fe/src/layout/BasicLayout/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="basic-layout">
+    <div class="app-header">
+      header
+    </div>
+    <div class="app-content">
+      <div class="left">
+        left
+      </div>
+      <div class="right">
+        right
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="./index.js"></script>
+
+<style lang="scss" scoped>
+  @import './index.scss';
+</style>
--- book-mgr-fe/src/router/index.js
+++ book-mgr-fe/src/router/index.js
@@ -3,8 +3,13 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 const routes = [
   {
     path: '/auth',
-    name: '/Auth',
+    name: 'Auth',
     component: () => import(/* webpackChunkName: "auth" */ '../views/Auth/index.vue')
+  },
+  {
+    path: '/',
+    name: 'BasicLayout',
+    component: ()=>import(/* webpackChunkName: "auth" */'../layout/BasicLayout/index.vue')
   }
 ];
 
